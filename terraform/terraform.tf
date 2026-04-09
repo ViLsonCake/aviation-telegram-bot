d84823eca93395de80575b89c5c568f14532de42
Tymofii Dmytrenko <timofeydmitrenko13@gmail.com>
2026-04-10 00:37:38 +0200

    Update Terraform config to save tf state to S3 backend

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -7,4 +7,10 @@ terraform {
   }
 
   required_version = ">= 1.2"
+
+  backend "s3" {
+    bucket = "aviation-telegram-bot-tfstate"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
