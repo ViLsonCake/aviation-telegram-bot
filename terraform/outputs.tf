f37426cb78c82819c8974908e4600343d6c2dda0
Tymofii Dmytrenko <timofeydmitrenko13@gmail.com>
2026-04-12 16:48:27 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -28,12 +28,32 @@ output "flights_notification_function_name" {
   value       = aws_lambda_function.flights_notification.function_name
 }
 
-output "flightradar_api_function_arn" {
-  description = "ARN of the flightradar-api Lambda function"
-  value       = aws_lambda_function.flightradar_api.arn
+output "flightradar_api_airport_function_arn" {
+  description = "ARN of the flightradar-api Airport Lambda function"
+  value       = aws_lambda_function.flightradar_api_airport.arn
 }
 
-output "flightradar_api_function_name" {
-  description = "Name of the flightradar-api Lambda function"
-  value       = aws_lambda_function.flightradar_api.function_name
+output "flightradar_api_airport_function_name" {
+  description = "Name of the flightradar-api Airport Lambda function"
+  value       = aws_lambda_function.flightradar_api_airport.function_name
+}
+
+output "flightradar_api_scheduled_flights_function_arn" {
+  description = "ARN of the flightradar-api Scheduled Flights Lambda function"
+  value       = aws_lambda_function.flightradar_api_scheduled_flights.arn
+}
+
+output "flightradar_api_scheduled_flights_function_name" {
+  description = "Name of the flightradar-api Scheduled Flights Lambda function"
+  value       = aws_lambda_function.flightradar_api_scheduled_flights.function_name
+}
+
+output "flightradar_api_specific_aircraft_function_arn" {
+  description = "ARN of the flightradar-api Specific Aircraft Lambda function"
+  value       = aws_lambda_function.flightradar_api_get_specific_aircraft.arn
+}
+
+output "flightradar_api_specific_aircraft_function_name" {
+  description = "Name of the flightradar-api Specific Aircraft Lambda function"
+  value       = aws_lambda_function.flightradar_api_get_specific_aircraft.function_name
 }
