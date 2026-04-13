4f43bee4f4fb30f7f5a5fb1ff91760dbb5d4839a
Tymofii Dmytrenko <timofeydmitrenko13@gmail.com>
2026-04-13 05:12:35 +0200

    Add @Param for the custom query

--- common-library/src/main/java/project/vilsoncake/common/repositories/AirportRepository.java
+++ common-library/src/main/java/project/vilsoncake/common/repositories/AirportRepository.java
@@ -2,6 +2,7 @@ package project.vilsoncake.common.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.vilsoncake.common.entities.AirportEntity;
 
 import java.util.Optional;
@@ -9,5 +10,5 @@ import java.util.Optional;
 public interface AirportRepository extends JpaRepository<AirportEntity, String> {
 
   @Query(value = "SELECT * FROM airports WHERE icao = :airportCode OR iata = :airportCode", nativeQuery = true)
-  Optional<AirportEntity> findByAirportCode(String airportCode);
+  Optional<AirportEntity> findByAirportCode(@Param("airportCode") String airportCode);
 }
