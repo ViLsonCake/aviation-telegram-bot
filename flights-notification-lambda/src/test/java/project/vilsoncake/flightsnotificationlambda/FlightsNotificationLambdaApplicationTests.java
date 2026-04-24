d89afd3c3331679f8384407c2409b6d0c1e04530
Tymofii Dmytrenko <timofeydmitrenko13@gmail.com>
2026-04-24 23:51:34 +0200

    Remove unused SpringBootTest annotation to fix CI/CD

--- flights-notification-lambda/src/test/java/project/vilsoncake/flightsnotificationlambda/FlightsNotificationLambdaApplicationTests.java
+++ flights-notification-lambda/src/test/java/project/vilsoncake/flightsnotificationlambda/FlightsNotificationLambdaApplicationTests.java
@@ -1,9 +1,7 @@
 package project.vilsoncake.flightsnotificationlambda;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FlightsNotificationLambdaApplicationTests {
 
   @Test
