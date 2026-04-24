7a1f898816cc4f4893b722d70685ab372ec90142
Tymofii Dmytrenko <timofeydmitrenko13@gmail.com>
2026-04-25 00:21:04 +0200

    Add error logging

--- flights-notification-lambda/src/main/java/project/vilsoncake/flightsnotificationlambda/handlers/FlightsNotificationTriggerHandler.java
+++ flights-notification-lambda/src/main/java/project/vilsoncake/flightsnotificationlambda/handlers/FlightsNotificationTriggerHandler.java
@@ -1,10 +1,12 @@
 package project.vilsoncake.flightsnotificationlambda.handlers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import project.vilsoncake.flightsnotificationlambda.models.FlightsNotificationTriggerPayload;
 import project.vilsoncake.flightsnotificationlambda.models.FlightsNotificationType;
 import project.vilsoncake.flightsnotificationlambda.services.adapters.NotificationTypeAdapter;
 
+@Slf4j
 @RequiredArgsConstructor(staticName = "create")
 public class FlightsNotificationTriggerHandler {
 
@@ -16,6 +18,7 @@ public class FlightsNotificationTriggerHandler {
 
       notificationTypeAdapter.process(type);
     } catch (Exception e) {
+      log.error("Error occurred while processing notification type", e);
       return "ERROR";
     }
 
